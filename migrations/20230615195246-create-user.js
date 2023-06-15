5ef86d319ad3ab8612d5d3abdb115f16c8d5ece2
francis-tg <franc9885@gmail.com>
2023-06-15 23:02:47 +0200

    add model

---
+++ migrations/20230615195246-create-user.js
@@ -0,0 +1,64 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nom: {
+        type: Sequelize.STRING
+      },
+      prenom: {
+        type: Sequelize.STRING
+      },
+      contact: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Roles",
+            schema:"schema"
+          },
+          key:"id"
+        }
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        defaultValue:true
+      },
+      id_section: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Sections",
+            schema:"schema"
+          },
+          key:"id"
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Role, {
+        foreignKey: "role",
+        onDelete:"CASCADE"
+      })
+      this.belongsTo(models.Section, {
+        foreignKey: "role",
+        onDelete:"CASCADE"
+      })
+    }
+  }
+  User.init({
+    nom: DataTypes.STRING,
+    prenom: DataTypes.STRING,
+    contact: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.INTEGER,
+    status: DataTypes.BOOLEAN,
+    id_section: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
