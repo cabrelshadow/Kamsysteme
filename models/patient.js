43dc890c0e64541bcbcc7fc49750a77d9e8c6b5a
cabrel sianou <sianou93@gmail.com>
2023-06-17 18:19:39 +0200

    add patient

--- models/patient.js
+++ models/patient.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
   class Patient extends Model {
     /**
      * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
+     * This method is not a part of Sequelize lifecycle., 
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
